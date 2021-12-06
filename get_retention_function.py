d1fd89eb3f2416ca941cc1701ec2821b59a891ff
Sehyun_Kim <nanica9191@naver.com>
2021-12-06 20:59:57 +0900

    Update get_retention_function.py

--- get_retention_function.py
+++ get_retention_function.py
@@ -13,27 +13,14 @@ def get_cohort_table(df):
     return df
 
 def get_cohort(df):
-    df = pd.merge(df,df.groupby(['user_id'])[['date']].min(), on ='user_id', how = 'inner')
-    df.columns = ['user_id', 'date', 'first_date']
-    first_order_month = pd.to_datetime(df['first_date']).dt.year*12 + pd.to_datetime(df['first_date']).dt.month
-    order_month = pd.to_datetime(df['date']).dt.year*12 + pd.to_datetime(df['date']).dt.month
-    df['cohort'] = order_month - first_order_month 
-    df['first_month'] = df['first_date'].str[:7]
+    df = get_cohort_table(df)
     cohort = df.groupby(['first_month','cohort'])[['user_id']].nunique().reset_index()
     cohort = cohort.pivot(index = 'first_month', columns = 'cohort' , values = 'user_id')
     cohort = (cohort.T/cohort[0]).T*100
     return cohort
 
 def get_retention(df):
-    df = pd.merge(df,df.groupby(['user_id'])[['date']].min(), on ='user_id', how = 'inner')
-    df.columns = ['user_id', 'date', 'first_date']
-    first_order_month = pd.to_datetime(df['first_date']).dt.year*12 + pd.to_datetime(df['first_date']).dt.month
-    order_month = pd.to_datetime(df['date']).dt.year*12 + pd.to_datetime(df['date']).dt.month
-    df['cohort'] = order_month - first_order_month 
-    df['first_month'] = df['first_date'].str[:7]
-    cohort = df.groupby(['first_month','cohort'])[['user_id']].nunique().reset_index()
-    cohort = cohort.pivot(index = 'first_month', columns = 'cohort' , values = 'user_id')
-    cohort = (cohort.T/cohort[0]).T*100
+    cohort = get_cohort(df)
     retention = pd.DataFrame(cohort.mean(), columns = ['retention']).T
     return retention
 
